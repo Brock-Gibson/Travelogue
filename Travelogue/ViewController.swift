a1f0a2316c504c6a59f8df1720790efc801fab66
Brock Gibson <brockgibson@Brocks-MacBook-Pro-2.local>
2019-04-19 13:11:38 -0500

    First Commit

---
+++ Travelogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travelogue
+//
+//  Created by Brock Gibson on 4/19/19.
+//  Copyright © 2019 Brock Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
